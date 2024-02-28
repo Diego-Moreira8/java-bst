766933897d5368522fc877f20a7ae1c9f6df3fc8
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-02-28 17:09:58 -0300

    feat: pretty print

--- Index.java
+++ Index.java
@@ -2,6 +2,6 @@ public class Index {
     public static void main(String[] args) {
         int[] arr = { 5, 2, 3, 1, 10, 0, -1, -100, 23243 };
         Tree myTree = new Tree(arr);
-        System.out.println(myTree.getRoot().getContent());
+        myTree.prettyPrint(myTree.getRoot(), "", true);
     }
 }
--- Tree.java
+++ Tree.java
@@ -23,12 +23,27 @@ public class Tree {
             int middleIndex = array.length / 2;
             int[] leftArray = Arrays.copyOfRange(array, 0, middleIndex);
             int[] rightArray = Arrays.copyOfRange(array, middleIndex + 1, array.length);
-    
+
             Node root = new Node(array[middleIndex]);
             root.setLeft(this.buildTree(leftArray));
             root.setRight(this.buildTree(rightArray));
-    
+
             return root;
         }
-      }
+    }
+
+    public void prettyPrint(Node node, String prefix, boolean isLeft) {
+        /*
+         * Prints the tree in a structured format.
+         * Made by the awesome people from TheOdinProject.com
+         */
+        if (node.getRight() != null) {
+            prettyPrint(node.getRight(), prefix + (isLeft ? "│   " : "    "), false);
+        }
+        System.out.println(prefix + (isLeft ? "└── " : "┌── ") + node.getContent());
+        if (node.getLeft() != null) {
+            prettyPrint(node.getLeft(), prefix + (isLeft ? "    " : "│   "), true);
+        }
+    }
+
 }
